52f2161a73e058fab8b76d3e70681e30c3f24382
Miguel Isaac <162846275+Owlazi@users.noreply.github.com>
2025-09-02 10:23:00 -0300

    Métodos toString

--- Exercicios/OrientacaoObjetos/BMetodos/Domain/Estudante.java
+++ Exercicios/OrientacaoObjetos/BMetodos/Domain/Estudante.java
@@ -5,9 +5,47 @@ public class Estudante {
     int idade;
     String sexo;
 
+    public Estudante() {
+
+    }
+
     public Estudante(String nome, int idade, String sexo) {
         this.nome = nome;
         this.idade = idade;
         this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Nome: %s
+                Idade: %d
+                Sexo: %s
+
+                """.formatted(nome, idade, sexo);
     }
 }
--- Exercicios/OrientacaoObjetos/BMetodos/Tests/EstudanteTest01.java
+++ Exercicios/OrientacaoObjetos/BMetodos/Tests/EstudanteTest01.java
@@ -1,7 +1,8 @@
 package Exercicios.OrientacaoObjetos.BMetodos.Tests;
+
 import java.util.Scanner;
 
-import Exercicios.OrientacaoObjetos.AClasses.Domain.Estudante;
+import Exercicios.OrientacaoObjetos.BMetodos.Domain.Estudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
@@ -9,17 +10,17 @@ public class EstudanteTest01 {
 
         Estudante estudante = new Estudante();
 
-
         System.out.println("Por favor, digite o nome:\n");
-        estudante.nome = sc.nextLine();
+        estudante.setNome(sc.nextLine());
 
         System.out.println("Por favor digite a idade:\n");
-        estudante.idade = sc.nextInt();
+        estudante.setIdade(sc.nextInt());
+        sc.nextLine();
 
         System.out.println("Por favor digite o sexo (M) para masculino e (F) para feminino");
-        estudante.sexo = sc.next().charAt(0);
+        estudante.setSexo(sc.nextLine());
 
-        System.out.println(estudante.toString());
+        System.out.println(estudante);
 
         sc.close();
 
